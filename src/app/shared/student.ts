01f04c845ba0607f2f1499e8eb0eb7366a5e4ca9
buddylee939 <buddylee939@hotmail.com>
2019-12-20 23:39:52 -0500

    Finished app

---
+++ src/app/shared/student.ts
@@ -0,0 +1,10 @@
+export class Student {
+  // tslint:disable-next-line: variable-name
+  _id: string;
+  studentName: string;
+  studentEmail: string;
+  section: string;
+  subjects: Array<string>;
+  dob: Date;
+  gender: string;
+}
